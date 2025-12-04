da89e94b6b5e7da1a49078dca1e8b8e1cf66d327
MD. Kauserul Islam <108425801+kausarul05@users.noreply.github.com>
2025-12-04 23:11:39 +0600

    url add

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect("mongodb+srv://chatbot24:WasbOsNZUDgWyliv@cluster0.mcdvihz.mongodb.net/?appName=Cluster0");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
