96dbb5ebda829eac932dd7b2fd3fed9d61f3d43a
MD. Kauserul Islam <108425801+kausarul05@users.noreply.github.com>
2025-12-04 23:22:24 +0600

    add jwt token

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+  return jwt.sign({ id }, "562ed3ae5adbfa3de23c60421e3cd70d3770c180eb63d644fc4133ad9e3c8ab6", { expiresIn: "7d" });
 };
 
 export default generateToken;
